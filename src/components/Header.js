f9670bc5d57061e00aefd71d193590853d97b4d0
Santosh Narayanan <santosh33@gmail.com>
2023-01-28 01:52:49 +0100

    Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import HighlightIcon from '@mui/icons-material/Highlight';
+
+const Header = () => {
+    return(
+        <header>
+            <h1>
+                <HighlightIcon />
+                ToDoList
+            </h1>
+        </header>
+    );
+};
+
+export default Header;
